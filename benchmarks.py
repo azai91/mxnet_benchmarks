ca63c7e92308eb938b9bf0a252c084f257821c03
Alex Zai <azai91@gmail.com>
2018-02-16 11:13:05 -0800

    use waitall to sync

--- benchmarks.py
+++ benchmarks.py
@@ -8,32 +8,35 @@ def warm_up():
     print('Allocating memory on GPUs')
     for gpu in gpus:
         print('Init on GPU', gpu)
-        a.as_in_context(gpu).wait_to_read()
+        a.as_in_context(gpu)
+    mx.nd.waitall()
 
 def allocate_gpu_memory_evenly():
-    a = mx.nd.random_normal(shape=(299, 300, 30, 60))
     start = time.time()
+    a = mx.nd.random_normal(shape=(299, 300, 30, 60))
     for i, gpu in enumerate(gpus):
         print('Init on GPU', i)
         gpu_start = time.time()
-        a.as_in_context(gpu).wait_to_read()
+        a.as_in_context(gpu)
         print('Time to init GPU', i, time.time() - gpu_start)
+    mx.nd.waitall()
     print('Total time to init all four GPUS', time.time() - start)
 
 def allocate_gpu_memory_one():
     start = time.time()
-    a = mx.nd.random_normal(shape=(299, 300, 30, 60), ctx=gpus[0])
+    a = mx.nd.random_normal(shape=(299, 300, 30, 60), ctx=gpus[0]) # this asychronous
     for i, gpu in enumerate(gpus):
         print('Init on GPU', i)
         gpu_start = time.time()
-        a.as_in_context(gpu).wait_to_read()
+        a.as_in_context(gpu)
         print('Time to init GPU', i, time.time() - gpu_start)
+    mx.nd.waitall()
     print('Total time to init all four GPUS', time.time() - start)
 
 
 def allocate_gpu_memory_async():
-    a = mx.nd.random_normal(shape=(299, 300, 30, 60))
     start = time.time()
+    a = mx.nd.random_normal(shape=(299, 300, 30, 60))  # this asychronous
     for i, gpu in enumerate(gpus):
         print('Init on GPU', i)
         gpu_start = time.time()
